4ae2e59f351235c474fee18920bf600c6edea018
Steven Checo <steven.checo.19@gmail.com>
2023-03-01 12:31:51 -0500

    first commit

---
+++ WebAppCICD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppCICD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
